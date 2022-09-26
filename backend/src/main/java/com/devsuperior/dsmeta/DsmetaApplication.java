a7dccce68ea104c7ae046c9d640a3709cd94fbb7
Marcos Vinicius <contato.marcoscandido@gmail.com>
2022-09-26 16:28:34 -0300

    Security config

--- backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DsmetaApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(DsmetaApplication.class, args);
+	
 	}
 
 }
