42cec810668b7da7fbd69225131037245a526be8
Marcos Vinicius <contato.marcoscandido@gmail.com>
2022-09-26 21:39:04 -0300

    Database

---
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dsmeta.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "tb_sales")
+public class Sale {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String sellerName;
+	private Integer visited;
+	private Integer deals;
+	private Double amount ;
+	private LocalDate date;
+	
+	public Sale() {
+	
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	
+
+}
