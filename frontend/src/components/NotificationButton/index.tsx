e42f7c7ee10147c096be1b58fc37218780e586df
Marcos Vinicius <contato.marcoscandido@gmail.com>
2022-10-14 21:51:33 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
+import { Toast } from 'react-toastify/dist/components';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 
@@ -13,7 +15,7 @@ function  handleClick(id: number){
 
   axios(`${BASE_URL}/sales/${id}/notification`)
   .then(response=>{
-    console.log("SUCESSO")
+        toast.info("SMS enviado com sucesso")
   });
 }
 
